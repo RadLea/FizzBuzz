9edaf9d5e3386c40e39b5168a374c6daa339f170
RadLea <radu.leanca@softwire.com>
2023-06-27 11:01:05 +0300

    corrected part 1

--- index.ts
+++ index.ts
@@ -2,14 +2,14 @@
 function fizzbuzz(): void {
 
     for(let i: number = 1; i <= 100; i++) {
-        if(i % 3 == 0 && i % 5 == 0) {
+        if (i % 3 == 0 && i % 5 == 0) {
             console.log("FizzBuzz");
-        }
-        if (i % 3 == 0) {
+        } else if (i % 3 == 0) {
             console.log("Fizz");
-        }
-        if (i % 5 == 0) {
+        } else if (i % 5 == 0) {
             console.log("Buzz");
+        } else {
+            console.log(i);
         }
     }
 }
