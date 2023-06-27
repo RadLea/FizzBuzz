d80496113f0153e7c8db209195014b26daaaf7ee
RadLea <radu.leanca@softwire.com>
2023-06-27 11:30:45 +0300

    part 2

--- index.ts
+++ index.ts
@@ -1,15 +1,44 @@
 // This is our main function
+
+function reverse4(text: string): string {
+    let newText: string = "";
+
+    while (text != "") {
+        newText += text.substring(text.length - 4);
+        text = text.substring(0, text.length - 4);
+    }
+
+    return newText;
+}
+
 function fizzbuzz(): void {
 
-    for(let i: number = 1; i <= 100; i++) {
-        if (i % 3 == 0 && i % 5 == 0) {
-            console.log("FizzBuzz");
-        } else if (i % 3 == 0) {
-            console.log("Fizz");
-        } else if (i % 5 == 0) {
-            console.log("Buzz");
-        } else {
+    for(let i: number = 1; i <= 1000; i++) {
+        let text: string = "";
+
+        if (i % 3 == 0) {
+            text += "Fizz";
+        }
+        if (i % 13 == 0) {
+            text += "Fezz";
+        }
+        if (i % 5 == 0) {
+            text += "Buzz";
+        }
+        if (i % 7 == 0) {
+            text += "Bang";
+        }
+        if (i % 11 == 0) {
+            text += "Bong";
+        }
+        if (i % 17 == 0) {
+           text = reverse4(text);
+        }
+
+        if (text == "") {
             console.log(i);
+        } else {
+            console.log(text);
         }
     }
 }
